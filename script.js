504b8f3dc7a49f87c50bb4f24ac516924649dd6d
Kaylin Flam <126748793+ceramicmushroom@users.noreply.github.com>
2023-08-17 17:42:23 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+const button = document.getElementById('myButton');
+
+button.addEventListener('click', function(){
+    const randomColor = generateRandomColor();
+    document.body.style.backgroundColor = randomColor;
+    });
+
+    function generateRandomColor() {
+        const letters = '0123456789ABCDEF';
+        let color = '#';
+        for (let i = 0; i < 6; i++) {
+            color+=letters[Math.floor(Math.random()*16)];
+        }
+        return color;
+    }
